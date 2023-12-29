e021483ed6aa890638ff33ad0e2f2b266a7986c3
DSI-DVJ <dsi@devincijunior.fr>
2023-12-29 16:50:25 +0100

    Small Visual Modifications

--- frontend/src/app/components/home-caroussel/home-caroussel.component.ts
+++ frontend/src/app/components/home-caroussel/home-caroussel.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../../auth.service";
+import {SharedService} from "../../shared.service";
 
 @Component({
   selector: 'app-home-caroussel',
   templateUrl: './home-caroussel.component.html',
   styleUrls: ['./home-caroussel.component.css']
 })
-export class HomeCarousselComponent {
+export class HomeCarousselComponent implements OnInit{
+  isLoggedIn: boolean = true;
+
+  constructor(private sharedService: SharedService) {}
+
+  ngOnInit() {
+    this.sharedService.loginStatus$.subscribe((loginSuccess) => {
+      this.isLoggedIn = !loginSuccess;
+    });
+  }
 
 }
